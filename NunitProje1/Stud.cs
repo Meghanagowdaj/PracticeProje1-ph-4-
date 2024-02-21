aa7ad9f758eddd0ba649cc0d23b96a7033507190
Megha P J <meghajayram30@gmail.com>
2024-02-21 13:19:53 +0530

    Add project files.

---
+++ NunitProje1/Stud.cs
@@ -0,0 +1,42 @@
+﻿using PracticeProje1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NunitProje1
+{
+    [TestFixture]
+    public class Stud
+    {
+        [Test]
+        public void TestStudentName()
+        {
+            // Arrange
+            Student student = new Student();
+            student.Name = "Megha";
+
+            // Act
+            string studentName = student.Name;
+
+            // Assert
+            Assert.AreEqual("Megha", studentName);
+        }
+
+        [Test]
+        public void TestStudentAddress()
+        {
+            // Arrange
+            Student student = new Student();
+            student.Address = "Tumkur(d) Kunigal";
+
+            // Act
+            string studentAddress = student.Address;
+
+            // Assert
+            Assert.AreEqual("Tumkur(d) Kunigal", studentAddress);
+        }
+    }
+}
+    
---
+++ PracticeProje1/Student.cs
@@ -0,0 +1,10 @@
+﻿namespace PracticeProje1
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Address { get; set; }
+
+    }
+}
